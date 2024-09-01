3b26efa438a749148639cdaa6f19715e0e22eb84
Afia Rahman <rahmanmou26@gmail.com>
2024-09-01 19:44:26 +0600

    added single card compo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,17 +1,38 @@
 import '../App.css';
 import { Todo } from '../model';
+import { CiEdit } from "react-icons/ci";
+import { MdOutlineDeleteOutline } from "react-icons/md";
+import { TiTick } from "react-icons/ti";
 
-interface Props{
+interface Props {
     todoList: Todo[];
     setTodoList: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList = ({todoList, setTodoList}: Props) => {
-  return (
-    <div>
-      
-    </div>
-  )
+const TodoList = ({ todoList, setTodoList }: Props) => {
+    const handleTick = (id:number) => {        
+        return setTodoList(todoList.map(todo => todo.id === id? {...todo, isDone: !todo.isDone}: todo))
+    }
+    return (
+        <div>
+            {
+                todoList.map(task => <form key={task.id} className='card'>
+                    <p>{task.todo}</p>
+                    <div className='action-bar'>
+                        <span className='icon'>
+                            <CiEdit />
+                        </span>
+                        <span className='icon'>
+                            <MdOutlineDeleteOutline />
+                        </span>
+                        <span className='icon' onClick={() => handleTick(task.id)}>
+                            <TiTick />
+                        </span>
+                    </div>
+                </form>)
+            }
+        </div>
+    )
 }
 
 export default TodoList
