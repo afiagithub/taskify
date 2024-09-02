44050630ed44f1493416f68409371d047526d158
Afia Rahman <rahmanmou26@gmail.com>
2024-09-02 22:02:27 +0600

    added delete function

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -13,16 +13,22 @@ const TodoList = ({ todoList, setTodoList }: Props) => {
     const handleTick = (id:number) => {        
         return setTodoList(todoList.map(todo => todo.id === id? {...todo, isDone: !todo.isDone}: todo))
     }
+
+    const handleDelete = (id:number) => {        
+        return setTodoList(todoList.filter(todo => todo.id !== id))
+    }
     return (
         <div>
             {
                 todoList.map(task => <form key={task.id} className='card'>
-                    <p>{task.todo}</p>
+                    {
+                        task.isDone? <s>{task.todo}</s> : <p>{task.todo}</p>
+                    }
                     <div className='action-bar'>
                         <span className='icon'>
                             <CiEdit />
                         </span>
-                        <span className='icon'>
+                        <span className='icon' onClick={() => handleDelete(task.id)}>
                             <MdOutlineDeleteOutline />
                         </span>
                         <span className='icon' onClick={() => handleTick(task.id)}>
